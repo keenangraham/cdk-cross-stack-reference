58b0914a4529990db771a4c92c7b4800365989e1
Keenan Graham <ksgraham@stanford.edu>
2022-07-20 19:10:45 -0700

    Add example of imported database and shared parameter

--- app.py
+++ app.py
@@ -18,11 +18,8 @@ from aws_cdk.aws_ec2 import SubnetSelection
 from aws_cdk.aws_ec2 import SubnetType
 from aws_cdk.aws_ssm import StringParameter
 
-
-CREATE_A = True
-CREATE_B = False
-USE_A = True
-USE_B = False
+from aws_cdk.aws_ec2 import SecurityGroup
+from aws_cdk.aws_secretsmanager import Secret
 
 
 class ConstructMultiplexer:
@@ -58,6 +55,7 @@ class ConstructMultiplexer:
             split_path = path.split('.')
             for split in split_path:
                 value = getattr(value, split)
+            print('export', path, value)
             parent_stack.export_value(value)
 
 
@@ -100,6 +98,42 @@ class PostgresConstruct(Construct):
         )
 
 
+class ExistingPostgresConstruct(Construct):
+
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            *,
+            instance_endpoint_address,
+            instance_identifier,
+            port,
+            security_group_id,
+            secret_complete_arn,
+            **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        security_group = SecurityGroup.from_lookup_by_id(
+            self,
+            'DBSecurityGroup',
+            security_group_id=security_group_id,
+        )
+        self.database = DatabaseInstance.from_database_instance_attributes(
+            self,
+            'Postgres',
+            instance_endpoint_address=instance_endpoint_address,
+            instance_identifier=instance_identifier,
+            port=port,
+            security_groups=[security_group],
+        )
+        secret = Secret.from_secret_complete_arn(
+            self,
+            'DBSecret',
+            secret_complete_arn=secret_complete_arn,
+        )
+        self.database.secret = secret
+
+
 class ProducerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -108,11 +142,30 @@ class ProducerStack(Stack):
             self,
             'VPCs'
         )
+        security_group = SecurityGroup.from_lookup_by_id(
+            self,
+            'DBSecurityGroup',
+            security_group_id='sg-067f520e33547bec2'
+        )
         config = [
             {
                 'on': True,
                 'construct': PostgresConstruct,
                 'construct_id': 'PostgresConstruct1',
+                'kwargs': {
+                    'vpcs': self.vpcs,
+                    'database_name': 'test',
+                    'allocated_storage': 6,
+                },
+                'export_values': [
+                    'database.instance_endpoint.hostname',
+                    'database.secret.secret_arn',
+                ],
+            },
+            {
+                'on': True,
+                'construct': PostgresConstruct,
+                'construct_id': 'PostgresConstruct2',
                 'kwargs': {
                     'vpcs': self.vpcs,
                     'database_name': 'test',
@@ -124,17 +177,17 @@ class ProducerStack(Stack):
                 ],
             },
             {
-                'on': False,
-                'construct': PostgresConstruct,
-                'construct_id': 'PostgresConstruct2',
+                'on': True,
+                'construct': ExistingPostgresConstruct,
+                'construct_id': 'ExistingPostgresConstruct',
                 'kwargs': {
-                    'vpcs': self.vpcs,
-                    'database_name': 'test',
-                    'allocated_storage': 5,
+                    'instance_endpoint_address': 'pp1sptwtgqzk3zc.cfkkcfbabiei.us-west-2.rds.amazonaws.com',
+                    'instance_identifier': 'pp1sptwtgqzk3zc',
+                    'port': 5432,
+                    'security_group_id': 'sg-067f520e33547bec2',
+                    'secret_complete_arn': 'arn:aws:secretsmanager:us-west-2:618537831167:secret:PostgresConstruct2PostgresS-hyzbJXu8QM1z-taDJI7',
                 },
                 'export_values': [
-                    'database.instance_endpoint.hostname',
-                    'database.secret.secret_arn',
                 ],
             }
         ]
@@ -149,19 +202,33 @@ class ConsumerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, producer: ProducerStack, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        resource = producer.multiplexer.resources.get('PostgresConstruct1')
+        resource = producer.multiplexer.resources.get('ExistingPostgresConstruct')
 
         hostname = StringParameter(
             self,
-            'Hostname2',
+            'Hostname',
             string_value=resource.database.instance_endpoint.hostname,
         )
 
-        secret = StringParameter(
+        secret_arn = StringParameter(
             self,
             'DBSecretARN',
             string_value=resource.database.secret.secret_arn,
         )
+
+        some_value = StringParameter.from_string_parameter_name(
+            self,
+            'SomeValue',
+            string_parameter_name='/test-branch/postgres/production/secret-arn'
+        )
+
+        copied_value = StringParameter(
+            self,
+            'CopiedValue',
+            string_value=some_value.string_value,
+            parameter_name='/some-branch/find/me/later',
+        )
+
 
 
 app = App()
